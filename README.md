511c57b7cba0515909ef7cbc5442b7cde299e8ea
vladislav.kori <vladislav.kori.work@gmail.com>
2023-05-22 01:20:35 +0500

    add prod mode for fronted

---
+++ README.md
@@ -0,0 +1,7 @@
+## AntiYellow
+
+![](./assets/preview1.png)
+
+``` 
+Blog for book "AntiYellow"
+``` 
--- frontend/src/configs/global.config.ts
+++ frontend/src/configs/global.config.ts
@@ -1,5 +1,12 @@
-export default {
+let result = {
     SERVER_HOST: 'http://localhost:3000',
     FRONT_HOST: 'http://localhost:5173',
-    DEFAULT_USER_AVATAR: 'https://avatars.mds.yandex.net/i?id=695aab42d706ce31b3c3240c9412aeb30b545cde-9086430-images-thumbs&n=13'
+};
+if (import.meta.env.PROD) {
+    result = {
+        SERVER_HOST: 'https://unithack.ru/:3000',
+        FRONT_HOST: 'https://unithack.ru/',
+    }
 }
+
+export default result;
